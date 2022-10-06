eaebaf2b1ded54dcebfe805e4bcabb8a62aec3e4
Lucas Oliveira Melo <lm4811@gmail.com>
2022-10-06 14:02:23 -0300

    task-13422: refactor: triggers features and repositories
    >CreateTrigger
    >FindTrigger
    >TriggerRepository
    >TriggerTypeRepository
    >Trigger
    >TriggerType
    >TriggerRequest
    >Task
    >TaskRepository

--- src/infra/repository/TriggersTypeRepository.ts
+++ src/infra/repository/TriggersTypeRepository.ts
@@ -10,16 +10,14 @@ export class PrismaTriggersType implements ITriggerTypesRepository {
     ){}
 
     async findById(id: number): Promise<TriggerType | null> {
-        const triggerTypeResult = await this.connection.query("SELECT * FROM tbl_trigger_type WHERE id = ?", [id]);
+        const triggerTypeResult = await this.connection.query("SELECT * FROM trigger.tbl_trigger_type WHERE id = ?", [id]);
 
         if(!triggerTypeResult)return null;
         
         return new TriggerType({
-                    description: triggerTypeResult.rows[0].description,
+                    name: triggerTypeResult.rows[0].name,
                     status: triggerTypeResult.rows[0].status,
-                    unitOfMeasurement: triggerTypeResult.rows[0].unitOfMeasurement,
-                    isAccumulated: triggerTypeResult.rows[0].isAccumulated,
-                    triggerId: triggerTypeResult.rows[0].id_trigger
+                    isProductiveTime: triggerTypeResult.rows[0].is_productive_time,
                 }, triggerTypeResult.rows[0].id);
             
     }
