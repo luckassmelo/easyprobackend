e2185ab800b1ef50ca871abebebd1bdc88fa229e
Lucas Melo <lucas.melo@schott.com>
2023-01-05 15:58:09 -0300

    task-16013: fix: error with .gitignore not working

--- dist/infra/api/middlewares/NotFound.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class NotFound {
-    async notFoundHandler(req, res) {
-        res.status(404).send('Route does not exist. 😞');
-    }
-}
-exports.default = NotFound;
-//# sourceMappingURL=NotFound.js.map
