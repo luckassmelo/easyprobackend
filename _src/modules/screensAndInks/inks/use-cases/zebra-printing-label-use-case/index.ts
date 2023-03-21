8de9770a114ec10b753c534616e87957a24a82d0
Carlos Magnani <carlos.magnani@schott.com>
2023-03-21 13:09:09 -0300

    delete import não utilizado

--- _src/modules/screensAndInks/inks/use-cases/zebra-printing-label-use-case/index.ts
+++ _src/modules/screensAndInks/inks/use-cases/zebra-printing-label-use-case/index.ts
@@ -1,6 +1,5 @@
 import { ZebraPrintingController } from "./zebra-printing-label.controller";
 import { ZebraPrintingUseCase } from "./zebra-printing-label.useCase";
-import { IZebraPrintingRepository } from "../../repository/zebra-printing.repository";
 import {ZebraPrintingRepository} from "../../../../../infra/database/respositories/zebra-printing.repository"
 const ZebraRepository = new ZebraPrintingRepository()
 const zebraPrintingUseCase = new ZebraPrintingUseCase(ZebraRepository)
