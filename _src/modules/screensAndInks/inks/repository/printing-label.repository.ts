79fdfd6847067f84a26d3b62b6331e8b2da65145
Carlos Magnani <carlos.magnani@schott.com>
2023-03-27 13:39:52 -0300

    task-17801:fix: Fixed coments

--- _src/modules/screensAndInks/inks/repository/printing-label.repository.ts
+++ _src/modules/screensAndInks/inks/repository/printing-label.repository.ts
@@ -1,5 +1,4 @@
 import { PrintingRegisterEntity } from "../entity/printing-register.entity";
-//Do a type for this method
 export interface IPrintingRepository {
     register(registerProps: PrintingRegisterEntity[]): Promise<any>
 }
