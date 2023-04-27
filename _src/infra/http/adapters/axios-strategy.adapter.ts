f6e2e5ea93a3571387cf332f68bf573c22455e8c
Lucas Melo <lucas.melo@schott.com>
2023-04-27 08:24:31 -0300

    task-17874: feat: included new pattern strategy to use on http request

---
+++ _src/infra/http/adapters/axios-strategy.adapter.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { AxiosRequestConfig } from 'axios';
+import { IHttpStrategy } from '../interface/http-strategy.interface';
+import { SSLAuth } from '../models/ssl-auth.model';
+import https from 'https';
+
+export class AxiosAdapterStrategy implements IHttpStrategy {
+  async request(
+    url: string,
+    method: string,
+    data?: any,
+    headers?: Record<string, string> | undefined,
+    auth?: SSLAuth | undefined): Promise<any> {
+
+    const config: AxiosRequestConfig = { url, method, data };
+    if (headers) config.headers = headers;
+    if (auth) config.httpsAgent = new https.Agent(auth.toAgentOptions());
+
+    const response = await axios(config);
+    return response.data;
+  }
+
+}
