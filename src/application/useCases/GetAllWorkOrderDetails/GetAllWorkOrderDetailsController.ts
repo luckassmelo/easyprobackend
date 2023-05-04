7170487642c09873fa910724dd1600244d31596f
Carlos Magnani <carlos.magnani@schott.com>
2023-05-04 13:31:56 -0300

    task-17878:feat: New generic decorator to save log

--- src/application/useCases/GetAllWorkOrderDetails/GetAllWorkOrderDetailsController.ts
+++ src/application/useCases/GetAllWorkOrderDetails/GetAllWorkOrderDetailsController.ts
@@ -1,11 +1,12 @@
 import { WorkOrderDetails } from "../../../domain/entities/WorkOrderDetails";
 import { GetAllWorkOrderDetailsUseCase } from "./GetAllWorkOrderDetailsUseCase";
-
-
+import { GenericLog } from "../../../../_src/domain/decorators/log.decorator";
+import {saveGenericLoggin} from "../../../../_src/infra/database/respositories/save-loggin.repository"
 
 export class GetAllWorkOrderDetailsController {
     constructor(private getAllWorkOrderDetailsUseCase: GetAllWorkOrderDetailsUseCase) {}
 
+    @GenericLog({info: { message: "Process to get all WorkOrdersDetails"}, process: "WorkOrdersDetails"}, saveGenericLoggin)
     async handle(): Promise<WorkOrderDetails[] | null> {
         return this.getAllWorkOrderDetailsUseCase.execute();
     }
--- src/application/useCases/Login/loginController.ts
+++ src/application/useCases/Login/loginController.ts
@@ -1,14 +1,17 @@
 import { LoginUseCase } from "./LoginUseCase";
-
+import { GenericLog } from "../../../../_src/domain/decorators/log.decorator";
+import { saveGenericLoggin } from "../../../../_src/infra/database/respositories/save-loggin.repository";
 export class LoginController {
     constructor(
         private LoginUseCase: LoginUseCase
     ){}
         
-        
+    @GenericLog({info: { message: "Token return"}, process: "Token response"}, saveGenericLoggin)   
     async handle(body: any): Promise<any | void> {
+        console.log(body);
         const { windowsuser, password} = body;
-
+        
+        
        
         
         try {
