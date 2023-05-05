8cfd8debdebf5c736ebbb36480f87f2acc66c11e
Lucas Melo <lucas.melo@schott.com>
2023-05-05 15:46:22 -0300

    task-17874: chore: rename folder to lower case

--- _src/modules/common/GetEasyProParameter/interfaces/get-easypro-parameter-repository.interface.ts
+++
@@ -1,5 +0,0 @@
-import { ParameterResponseProps } from '../types/param.types';
-
-export interface IGetEasyPROParameterRepository {
-  getParameter(idSite: number, name: string): Promise<ParameterResponseProps[]>;
-}
