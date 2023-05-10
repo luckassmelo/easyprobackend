7ff2f4f7af02f70cf53ea15fdf48539e9c494a12
Carlos Magnani <carlos.magnani@schott.com>
2023-05-10 10:53:18 -0300

    changes to decorator log

--- _src/domain/decorators/log.decorator.ts
+++ _src/domain/decorators/log.decorator.ts
@@ -1,4 +1,3 @@
-import { Request, Response, NextFunction} from "express";
 import {ILogginDecorator} from '../interface/ILogginDecorator.interface'
 
 type logProps = {
@@ -21,7 +20,6 @@ type logProps = {
         value: async function (...args: any[]) {
           const result = await originalMethod.apply(this, args);
           
-          const req = args.find(arg => arg?.constructor?.name === 'IncomingMessage');
           const res = args.find(arg => arg?.constructor?.name === 'ServerResponse');
           
           const logData = {
@@ -33,7 +31,7 @@ type logProps = {
             await logservice.saveLog(formatJSON, props.process);
           } catch (error) {
             const logData = {
-              statusCode: res?.statusCode ?? 200,
+              statusCode: res?.statusCode ?? 500,
               message: error,
             };
             const formatJSON = JSON.parse(JSON.stringify(logData))
@@ -47,45 +45,4 @@ type logProps = {
       return newDescriptor;
     }
   }  
-  // export function GenericLog(props: logProps, logservice: ILogginDecorator) {
-  //   return function (
-  //     target: any,
-  //     propertyKey: string,
-  //     descriptor: TypedPropertyDescriptor<any>
-  //   ) {
-  //     const originalMethod = descriptor.value ;
-  //     console.log(originalMethod);
-
-      
-  //     const newDescriptor: TypedPropertyDescriptor<any> = {
-  //       ...descriptor,
-  //       async value(req: Request, res: Response, next: NextFunction, ...args: any[]){
-  //           try {
-  //               const result = originalMethod
-  //               const logData = {
-  //                   statusCode: req?.statusCode ?? 200,
-  //                   message: props.info,
-  //               }
-  //               const jsonString = JSON.stringify(logData)
-  //               const jsonFormat = JSON.parse(jsonString)    
-  //               await logservice.saveLog(jsonFormat, props.process)
-  //               return result
-  //             } catch (error) {
-  //               const statusCode = req.statusCode ?? 500;
-  //               const message = error.response?.data?.message || error.message;
-  //               const logData = {
-  //                   statusCode: statusCode,
-  //                   message: message,
-  //               }
-  //               const jsonString = JSON.stringify(logData)
-  //               const jsonFormat = JSON.parse(jsonString)                
-  //               await logservice.saveLog(jsonFormat, props.process)
-  //               next(error);
-  //             }
-  //           }
-  //         };
-          
-  //         return newDescriptor;
-  //       }
-  //     }
    
