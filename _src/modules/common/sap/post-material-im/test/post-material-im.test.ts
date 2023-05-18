934ccbb5612cefdd3b9ffec94ba6f6cd3cc58844
Lucas Melo <lucas.melo@schott.com>
2023-05-18 16:30:56 -0300

    task-17875: included test to post material im

---
+++ _src/modules/common/sap/post-material-im/test/post-material-im.test.ts
@@ -0,0 +1,44 @@
+import { PostMaterialSAPIM } from '../models/post-material-sap-im.model';
+import { sapConnectionConfigurationRfc } from '../../../../../config/config';
+import { RfcAdapter } from '../../../../../infra/SAP/adapters/rfc.adapter';
+
+describe('SAP post materials', () => {
+  const TIMEOUT_IN_MILLISECONDS = 40000;
+  const rfcConnection = new RfcAdapter({ ...sapConnectionConfigurationRfc });
+
+  beforeAll(async () => {
+    await rfcConnection.connect('PT');
+  }, TIMEOUT_IN_MILLISECONDS);
+
+
+  afterAll(async () => {
+    await rfcConnection.close();
+  }, TIMEOUT_IN_MILLISECONDS);
+
+  test('should be returning a true to be connected with SAP', async () => {
+    expect(await rfcConnection.connect('PT')).toBeTruthy();
+  }, TIMEOUT_IN_MILLISECONDS);
+
+  test('should post material in SAP (IM)', async () => {
+    const postMaterialIM = new PostMaterialSAPIM({
+      materialNumber: '6313662',
+      plantCode: '3600',
+      locationCode: '0603',
+      quantity: 1,
+      unityOfMeasurement: 'ST',
+      movementType: '201',
+      costCenter: 'C122222805',
+      internalOrder: '',
+      batch: '',
+      bin: 'FA07',
+      headerDescription: '',
+      referenceDocument: '',
+      language: 'PT'
+    });
+
+    const responseIM = await rfcConnection.postMaterialIM(postMaterialIM);
+
+    expect(responseIM['document']).not.toEqual('');
+
+  }, TIMEOUT_IN_MILLISECONDS);
+});
