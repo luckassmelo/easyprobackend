5fe814662cdf45a6235d9bb4686595e0190b2d11
Lucas Melo <lucas.melo@schott.com>
2023-05-29 16:44:52 -0300

    task-18754: refactor: included new connection type for pgboss using singleton pattern

--- _src/modules/common/queue/create-job/repository/create-job.repository.ts
+++ _src/modules/common/queue/create-job/repository/create-job.repository.ts
@@ -1,18 +1,17 @@
-import { PgBossAdapter, pgBossConnection } from '../../../../../infra/pgboss/adapter/pgboss.adapter';
 import { ICreateJobRepository } from '../interfaces/create-job-repository.interface';
 import { GenericError } from '../../../../../infra/api/errors/generic.error';
 import { CreateJob } from '../models/create-job.model';
+import { PgBossConnectionSingleton } from '../../../../../infra/database/connections/pgboss.connection';
 
 export class CreateJobRepository implements ICreateJobRepository {
-  constructor(private adapter: PgBossAdapter) { }
+  constructor() { }
 
 
   async createJob(job: CreateJob): Promise<CreateJob> {
-    await pgBossConnection.start();
-    const id = await this.adapter.createJob(job.props.jobName, job.props.data);
+    const instance = await PgBossConnectionSingleton.getInstance();
+    const id = await instance.createJob(job.props.jobName, job.props.data);
 
     if (id === null) throw new GenericError("Error to create job", "Anything is wrong with pgBoss connection", 500);
-    await pgBossConnection.close();
 
     return new CreateJob(job.props, id);
   }
