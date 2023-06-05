679194344d58028cbc479fd09010fd6984157eab
Carlos Magnani <carlos.magnani@schott.com>
2023-06-05 09:42:40 -0300

    catch error into repository

--- _src/modules/screens-and-inks/paint/formula/insert-formula/repository/insert-formula.repository.ts
+++ _src/modules/screens-and-inks/paint/formula/insert-formula/repository/insert-formula.repository.ts
@@ -8,29 +8,34 @@ import { ResponseFormula } from "../types/response-insert-formula.type";
 export class InsertFormulaRepository implements IinsertFormulaRepository{
     constructor(private readonly adapter: PostgresSQLAdapter){}
 
+    
     async insertFormulaInfo(formula: FormulaInfos): Promise<ResponseFormula | BadRequestError> {
-        
-        await this.adapter.connection('paint.tbl_formula')
-        .insert({
-            description: formula.descriptionGetter,
-            sap_material_number: formula.sapNumberGetter,
-            vendor_description: formula.vendorDescGetter,
-            medium_description: formula.mediumDescGetter,
-            id_type: formula.usageTypeGetter,
-            id_color: formula.colorGetter,
-            ink_weight: formula.inkWeightGetter,
-            medium_weight: formula.mediumWeightGetter,
-            min_viscosity: formula.minViscosityGetter,
-            max_viscosity: formula.maxViscosityGetter,
-            min_density: formula.minDensityGetter,
-            max_density: formula.maxDensityGetter,
-            status: formula.statusGetter,
-            site: formula.siteGetter
-        });
+    
+        try{
+            await this.adapter.connection('paint.tbl_formula')
+                .insert({
+                    description: formula.descriptionGetter,
+                    sap_material_number: formula.sapNumberGetter,
+                    vendor_description: formula.vendorDescGetter,
+                    medium_description: formula.mediumDescGetter,
+                    id_type: formula.usageTypeGetter,
+                    id_color: formula.colorGetter,
+                    ink_weight: formula.inkWeightGetter,
+                    medium_weight: formula.mediumWeightGetter,
+                    min_viscosity: formula.minViscosityGetter,
+                    max_viscosity: formula.maxViscosityGetter,
+                    min_density: formula.minDensityGetter,
+                    max_density: formula.maxDensityGetter,
+                    status: formula.statusGetter,
+                    site: formula.siteGetter
+                });
 
 
-        return {
-            description: `Success with save ${formula.descriptionGetter}` 
-        }
+            return {
+                description: `Success with save ${formula.descriptionGetter}` 
+            }
+    }catch(error){
+        throw new BadRequestError();
+    }
     }
 }
