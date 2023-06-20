c51a270e073c53f8d5aa4878082ccf90bec3a7f7
Carlos Magnani <carlos.magnani@schott.com>
2023-06-20 13:59:48 -0300

    changes into test file

--- _src/modules/screens-and-inks/paint/formula/insert-formula/test/insert-formula.test.ts
+++ _src/modules/screens-and-inks/paint/formula/insert-formula/test/insert-formula.test.ts
@@ -26,7 +26,23 @@ describe("Insert formula into repository", () => {
                 site: 2
             };
         
-        const initialProps = new FormulaInfos(formulaProps);
+        const initialProps = new FormulaInfos();
+
+            initialProps.description = formulaProps.description;
+            initialProps.sapNumber = formulaProps.sapNumber;
+            initialProps.vendorDesc = formulaProps.vendorDesc;
+            initialProps.mediumDesc = formulaProps.mediumDesc;
+            initialProps.usageType = formulaProps.usageType;
+            initialProps.color = formulaProps.color;
+            initialProps.inkWeight = formulaProps.inkWeight;
+            initialProps.mediumWeight = formulaProps.mediumWeight;
+            initialProps.minViscosity = formulaProps.minViscosity;
+            initialProps.maxViscosity = formulaProps.maxViscosity;
+            initialProps.minDensity = formulaProps.minDensity;
+            initialProps.maxDensity = formulaProps.maxDensity;
+            initialProps.status = formulaProps.status;
+            initialProps.site = formulaProps.site;
+            
         insertFormulaRepository["formulaBase"].push(initialProps);
 
         const result = insertFormulaRepository.insertFormulaInfo(initialProps);
